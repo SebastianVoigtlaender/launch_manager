883dbb80f17fe61017b799f5217d55603ec24087
Lolkaul-merlin <Lolkaul-merlin@users.noreply.github.com>
2025-10-31 10:41:59 +0000

    though it is supported audio.

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,3 +1,5 @@
 // environment-specific settings
 
 # Touch update: 1761001655
+
+# Touch update: 1761001655
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
 // webpack configuration for launch_manager
 
 # Touch update: 1761001654
+
+# Touch update: 1761001655
